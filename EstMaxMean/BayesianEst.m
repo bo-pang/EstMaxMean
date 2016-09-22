a159321235a041a3b1a3f35938f45f16155a13e3
pbBoPang <buaapangbo@gmail.com>
2016-09-21 18:03:20 -0600

    Codes for other estimators

---
+++ EstMaxMean/BayesianEst.m
@@ -0,0 +1,18 @@
+function MSE=BayesianEst(a,mu,var)
+N=1;
+Rept=2000;
+MSE=0;
+
+for i=1:Rept
+   x=normrnd(mu,var,1,N);
+   % Posterior
+   muP=sum(x)/(var^2/a^2+N);
+   varP=(1/(1/a^2+N/var^2))^(0.5);
+   % Expectation of |\mu| with respect to posterior distribution
+   fun=@(y,Mu,Var) abs(y).*GaussianFunc(Mu,Var,y);
+   AbsMu=integral(@(y)fun(y,muP,varP),-Inf,Inf);
+   
+   MSE=MSE+(AbsMu-abs(mu))^2*GaussianFunc(mu,var,x);
+end
+
+end
